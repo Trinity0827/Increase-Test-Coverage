e9f3619146c552006b173153854d99da5b443d9b
Trinity Washington <122378682+Trinity0827@users.noreply.github.com>
2025-11-14 15:54:16 +0000

    passing

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -15,4 +15,45 @@ public class AtmTests
         Assert.True(result);
         Assert.Equal(75, testAtm.getBalance());
     }
+
+    [Fact]
+        public void Withdraw_Fails_When_Not_Enough()
+        {
+            Atm atm = new Atm(100);
+
+            bool result = atm.withdraw(200);
+
+            Assert.False(result);
+            Assert.Equal(100, atm.getBalance());
+        }
+
+        [Fact]
+        public void Deposit_Works_With_Positive_Number()
+        {
+            Atm atm = new Atm(100);
+
+            bool result = atm.deposit(50);
+
+            Assert.True(result);
+            Assert.Equal(150, atm.getBalance());
+        }
+
+        [Fact]
+        public void Deposit_Fails_With_Negative_Number()
+        {
+            Atm atm = new Atm(100);
+
+            bool result = atm.deposit(-10);
+
+            Assert.False(result);
+            Assert.Equal(100, atm.getBalance());
+        }
+
+        [Fact]
+        public void GetBalance_Returns_Current_Balance()
+        {
+            Atm atm = new Atm(100);
+
+            Assert.Equal(100, atm.getBalance());
+        }
 }
